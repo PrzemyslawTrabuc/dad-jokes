0c2a69945419c8513378cb1f53eae43cf82648b4
Przemek <czemek17@gmail.com>
2022-09-22 18:18:22 +0200

    reinit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  return <Header></Header>;
+}
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <h1>
+        Welocme to <span className="text-amber-300">Dad Jokes</span>
+      </h1>
+    </div>
+  );
+}
+
+export default Header;
