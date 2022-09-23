e2da51c5a732bc67fd22e793d75d21fe37f18fd6
Przemek <czemek17@gmail.com>
2022-09-23 15:56:44 +0200

    added test fetch from api

--- src/pages/RandomJoke.tsx
+++ src/pages/RandomJoke.tsx
@@ -1,7 +1,34 @@
+import { useEffect, useState } from "react";
 import Title from "../components/Title";
 
 function RandomJoke() {
-  return <Title title="Dad Joke" />;
+  const [dadJokesCount, setDadJokesCount] = useState<number | null>(null);
+  const getRandomJokes = async () => {
+    const response = await fetch(
+      "https://dad-jokes.p.rapidapi.com/joke/count",
+      {
+        headers: {
+          "X-RapidAPI-Key":
+            "6bc20974eamshab178251bbf4d6dp1ccf5ejsn15ea6633a3e8",
+          "X-RapidAPI-Host": "dad-jokes.p.rapidapi.com",
+        },
+        method: "GET",
+      }
+    );
+    const data = await response.json();
+    setDadJokesCount(data.body);
+  };
+
+  useEffect(() => {
+    getRandomJokes();
+  }, []);
+
+  return (
+    <>
+      <Title title="Dad Joke" />
+      <div>Dad Jokes count {dadJokesCount}</div>
+    </>
+  );
 }
 
 export default RandomJoke;
