517efeec6fd7fbef51bb2f2dc3ce9854495528d0
przemyslawtrabuc <czemek17@gmail.com>
2022-09-29 22:41:31 +0200

    changed MyButton min width

--- src/components/MyButton.tsx
+++ src/components/MyButton.tsx
@@ -7,7 +7,7 @@ function MyButton({ handleClick, buttonText, isFloating }: ImyButtonProps) {
   return (
     <button
       onClick={handleClick}
-      className={`dark:bg-fuchsia-800 w-auto] p-3 rounded-2xl dark:hover:bg-fuchsia-700 bg-fuchsia-300 hover:bg-fuchsia-200 ${
+      className={`dark:bg-fuchsia-800 w-auto] p-3 rounded-2xl dark:hover:bg-fuchsia-700 bg-fuchsia-300 hover:bg-fuchsia-200 min-w-[150px] ${
         isFloating ? "animate-bounce" : ""
       }`}
     >
