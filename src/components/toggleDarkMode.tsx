4848db3adc72a15cbb16e4ceb34182f8dbc126e1
przemyslawtrabuc <czemek17@gmail.com>
2022-10-07 20:24:37 +0200

    deleted unusen imports

--- src/components/toggleDarkMode.tsx
+++ src/components/toggleDarkMode.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import { useContext } from "react";
 import { ThemeContext } from "../context/context";
 import { SunIcon, MoonIcon } from "@heroicons/react/24/outline";
 
